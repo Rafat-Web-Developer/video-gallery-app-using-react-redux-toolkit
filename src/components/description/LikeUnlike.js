7f5f291028f88bf0f23b72eeaeb0b412b41df7ea
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-09-02 19:15:47 +0600

    Complete Full Project

--- src/components/description/LikeUnlike.js
+++ src/components/description/LikeUnlike.js
@@ -1,25 +1,31 @@
 import likeImage from "../../assets/like.svg";
 import unlikeImage from "../../assets/unlike.svg";
 
-export default function LikeUnlike() {
-    return (
-        <div class="flex gap-10 w-48">
-            <div class="flex gap-1">
-                <div class="shrink-0">
-                    <img class="w-5 block" src={likeImage} alt="Like" />
-                </div>
-                <div class="text-sm leading-[1.7142857] text-slate-600">
-                    100K
-                </div>
-            </div>
-            <div class="flex gap-1">
-                <div class="shrink-0">
-                    <img class="w-5 block" src={unlikeImage} alt="Unlike" />
-                </div>
-                <div class="text-sm leading-[1.7142857] text-slate-600">
-                    100K
-                </div>
-            </div>
+export default function LikeUnlike({ like, unlike }) {
+  return (
+    <div className='flex gap-10 w-48'>
+      <div className='flex gap-1'>
+        <div className='shrink-0'>
+          <img
+            className='w-5 block cursor-pointer'
+            src={likeImage}
+            alt='Like'
+          />
         </div>
-    );
+        <div className='text-sm leading-[1.7142857] text-slate-600'>{like}</div>
+      </div>
+      <div className='flex gap-1'>
+        <div className='shrink-0'>
+          <img
+            className='w-5 block cursor-pointer'
+            src={unlikeImage}
+            alt='Unlike'
+          />
+        </div>
+        <div className='text-sm leading-[1.7142857] text-slate-600'>
+          {unlike}
+        </div>
+      </div>
+    </div>
+  );
 }
