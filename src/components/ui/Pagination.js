71b2ade47c8425ad18581bb19a51777b06596428
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-09-03 18:07:01 +0600

    Complete Page Number Content

--- src/components/ui/Pagination.js
+++ src/components/ui/Pagination.js
@@ -1,20 +1,26 @@
+import { useSelector } from "react-redux";
+
 export default function Pagination() {
-  // ?_start=5&_limit=10
+  const { videos } = useSelector((state) => state.videos);
+  const pageNumber = Math.ceil(videos.length / 5) || 0;
+  // console.log(pageNumber);
+  const getPageContent = () => {
+    let content = [];
+    for (let i = 0; i < pageNumber; i++) {
+      // const item = animals[idx];
+      content.push(
+        <div className='bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer'>
+          {i + 1}
+        </div>
+      );
+    }
+    return content;
+  };
+  // ?_page=2&_limit=5
   return (
     <section className='pt-12'>
       <div className='max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 justify-end'>
-        <div className='bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer'>
-          1
-        </div>
-        <div className='bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer'>
-          2
-        </div>
-        <div className='bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer'>
-          3
-        </div>
-        <div className='bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer'>
-          4
-        </div>
+        {getPageContent()}
       </div>
     </section>
   );
